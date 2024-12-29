109230e6bb3ec0593bc0e7556d136570b84e4a62
Prabin70 <xthaprabin125@gmail.com>
2024-12-29 14:00:08 +0545

    supabase with next

--- src/utils/supabase/server.js
+++ src/utils/supabase/server.js
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
+import { config } from "dotenv";
+import path from 'path';
 
+config({ path: '../../../.env' });
 
-const supabaseUrl = "https://zcolkplfjismxyilacsb.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpjb2xrcGxmamlzbXh5aWxhY3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0MDMzNzksImV4cCI6MjA1MDk3OTM3OX0.t9I-g7-3zrqMnK20VbjabNiXfQ3PNvYG0Hhri5Mqncg"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
